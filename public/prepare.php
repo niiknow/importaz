8dab2f5c571c1e4d7e8b31b4e8fc3489017bd420
Thanh Nguyen <Thanh Nguyen>
2016-11-19 08:58:42 -0600

    update restriction

--- public/prepare.php
+++ public/prepare.php
@@ -2,14 +2,7 @@
 // Define path to application directory
 defined('APP_PATH')
 || define('APP_PATH', realpath(dirname(__FILE__) . '/../'));
-// Define application environment: dev, tst, uat, stg, prd
-defined('APP_ENV')
-|| define('APP_ENV', (getenv('APP_ENV') ? getenv('APP_ENV') : 'prd'));
-// enable debug
-if (APP_ENV !== 'prd') {
-    ini_set('display_errors', 'On');
-    error_reporting(E_ALL | E_STRICT);
-}
+
 if (PHP_SAPI === 'cli-server') {
     // To help the built-in PHP dev server, check if the request was actually for
     // something which should probably be served as a static file
